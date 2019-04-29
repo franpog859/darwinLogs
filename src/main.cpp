86e465ffc8b03fb9d829142bd657cad93be189bc
franpog859 <franciszek.pog@gmail.com>
2019-04-29 21:23:38 +0200

    Add working example of plotting

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@
 #include "IoService.hpp"
 #include "LogService.hpp"
 
+#include <map>
+#include <vector>
+#include <cmath>
+
+#include "../lib/gnuplot-iostream.h"
+
 //#define _CRTDBG_MAP_ALLOC // Comment this if you are not debugging it on Windows.
 //#include <stdlib.h> // Comment this if you are not debugging it on Windows.
 //#include <crtdbg.h> // Comment this if you are not debugging it on Windows.
@@ -29,6 +35,31 @@ int main(int argc, char *argv[]) {
 		std::vector<Info> logs = LogService::getLogs();
 		IoService::saveLogs(&logs, &params); //TODO: This should save logs to CSV file.
 		//IoService::generateGraphs(LogService::getLogs(), &params);
+	}
+
+
+	{
+		Gnuplot gp;
+
+		std::vector<std::pair<double, double> > xy_pts_A;
+		for(double x=-2; x<2; x+=0.01) {
+			double y = x*x*x;
+			xy_pts_A.push_back(std::make_pair(x, y));
+		}
+
+		std::vector<std::pair<double, double> > xy_pts_B;
+		for(double alpha=0; alpha<1; alpha+=1.0/24.0) {
+			double theta = alpha*2.0*3.14159;
+			xy_pts_B.push_back(std::make_pair(cos(theta), sin(theta)));
+		}
+
+		gp << "set xrange [-2:2]\nset yrange [-2:2]\n";
+		// Data will be sent via a temporary file.  These are erased when you call
+		// gp.clearTmpfiles() or when gp goes out of scope.  If you pass a filename
+		// (e.g. "gp.file1d(pts, 'mydata.dat')"), then the named file will be created
+		// and won't be deleted (this is useful when creating a script).
+		gp << "plot" << gp.file1d(xy_pts_A, "fileA.dat") << "with lines title 'cubic',"
+			<< gp.file1d(xy_pts_B) << "with points title 'circle'" << std::endl;
 	}
 	return 0;
 }
