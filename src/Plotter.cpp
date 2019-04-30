ee44e68ed2cb4aea05bfd788d32205ea56516f55
franpog859 <franciszek.pog@gmail.com>
2019-04-30 13:20:58 +0200

    Save actually first graph

--- src/Plotter.cpp
+++ src/Plotter.cpp
@@ -3,16 +3,21 @@
 
 void Plotter::saveGraphs(std::vector<Info> *info, Parameters *parameters) {
     Gnuplot gp;
-
     gp << "set terminal png\n";
-    gp << "set output 'my_graph_1.png'\n";
 
-    std::vector<std::pair<double, double> > xy_pts_A;
-    for(double x=-2; x<2; x+=0.01) {
-        double y = x*x*x;
-        xy_pts_A.push_back(std::make_pair(x, y));
+    gp << "set output 'corelation-dexterity.png'\n";
+    std::vector<std::pair<int, int>> xy_avarage_dexterity;
+    for(int i = 0; i < info->size(); i++) {
+        xy_avarage_dexterity.push_back(std::make_pair(i, info->at(i).averageStatistics.dexterity));
     }
 
-    gp << "set xrange [-2:2]\nset yrange [-2:2]\n";
-    gp << "plot" << gp.file1d(xy_pts_A) << "with lines title 'cubic'" << std::endl;
+    std::vector<std::pair<int, int>> xy_minimal_survival_dexterity;
+    for(int i = 0; i < info->size(); i++) {
+        xy_minimal_survival_dexterity.push_back(std::make_pair(i, info->at(i).minimalSurvivalStatistics.dexterity));
+    }
+
+    gp << "plot '-' with lines title 'avarage dexterity', ";
+	gp << "'-' with lines title 'minimal survival dexterity'\n";
+	gp.send1d(xy_avarage_dexterity);
+	gp.send1d(xy_minimal_survival_dexterity);
 }
