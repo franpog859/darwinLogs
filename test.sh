45dc04157013e1cbd7c4c1a3d1054a506361a754
franpog859 <franciszek.pog@gmail.com>
2019-05-03 13:42:15 +0200

    Add memory leaks checking with Valgind

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -o errexit ; set -o nounset
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${CURRENT_DIR}
 
-echo "Run cmake . ..."
+echo "Running cmake . ..."
 cmake .
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -13,8 +13,17 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     export CPLUS_INCLUDE_PATH=/usr/local/Cellar/boost/1.69.0_2/include/ #Be sure that it's correct version.
 fi
 
-echo "Run make..."
+echo "Running make..."
 make
 
-echo "Run example usage..."
-./darwinLogs -e=12 -ie=data/env.json -ip=data/pop.json -op=data/output/
+echo "Running example usage..." #Check if it actually works on MACOS
+echo "To check memory leaks run: cat valgrind-out.txt"
+valgrind --leak-check=full \
+         --show-leak-kinds=all \
+         --track-origins=yes \
+         --verbose \
+         --log-file=valgrind-out.txt \
+         ./darwinLogs -e=12 -ie=data/env.json -ip=data/pop.json -op=data/output/
+
+echo "Checking memory leaks..."
+cat valgrind-out.txt | grep ==
